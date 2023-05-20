b5ad034659e94d99a428fbc23d3fe366219ce508
kala-111 <113574035+kala-111@users.noreply.github.com>
2023-05-20 13:56:41 +0530

    Add files via upload

--- Ginproject1/main.go
+++ Ginproject1/main.go
@@ -1,51 +1,55 @@
 package main
+
 import (
 	"net/http"
-	"gin.com/gin-gonic/gin"
+
+	"github.com/gin-gonic/gin"
 )
-type Weather struct{
 
-	 ID             string 'json:"id"'
-	 Name           string 'json:"name"'
-	 AirTemparature string 'json:"airtempareture"'
-	 Humidity       string 'json:"humidity"'
-	 windspeed      string 'json:"windspeed"'  
-	}
-	var weatherdevice =[]weather{
-		{ID:"1",Name:"vijayawada",AirTemparature:"40",Humidity:"25%",windspeed:"10 km/h"}
-		{ID:"2",Name:"kurnool",AirTemparature:"35",Humidity:"20%",windspeed:"11 km/h"}
-		{ID:"3",Name:"vizag",AirTemparature:"40",Humidity:"30%",windspeed:"20 km/h"}
-	}
-	func getweatherdevice(context *gin.context){
-		context.JOSN(http.StatusOK,weather)
-	}
+type Weather struct {
+	ID             string `json:"id"`
+	Name           string `json:"name"`
+	AirTemparature string `json:"airtempareture"`
+	Humidity       string `json:"humidity"`
+	windspeed      string `json:"windspeed"`
+}
 
-	func addweatherdevice(context *gin.context){
-		var newdevice Weather  
-		if err :=context.BindJSON(&newdevice);err!=nil{
+var weatherdevice = []Weather{
+	{ID: "1", Name: "vijayawada", AirTemparature: "40", Humidity: "25%", windspeed: "10 km/h"},
+	{ID: "2", Name: "kurnool", AirTemparature: "35", Humidity: "20%", windspeed: "11 km/h"},
+	{ID: "3", Name: "vizag", AirTemparature: "40", Humidity: "30%", windspeed: "20 km/h"},
+}
+
+func getweatherdevice(context *gin.Context) {
+	context.JSON(http.StatusOK, weatherdevice)
+}
+
+func addweatherdevice(context *gin.Context) {
+	var newdevice Weather
+	if err := context.BindJSON(&newdevice); err != nil {
+		return
+	}
+	weatherdevice = append(weatherdevice, newdevice)
+
+	context.IndentedJSON(http.StatusCreated, newdevice)
+}
+
+func deletebyId(context *gin.Context) {
+	id := context.Param("id")
+	for i, p := range weatherdevice {
+		if p.ID == id {
+			weatherdevice = append(weatherdevice[:i], weatherdevice[i+1:]...)
+			context.IndentedJSON(http.StatusOK, gin.H{"message": "deleted"})
 			return
 		}
-		weatherdevice=append(weatherdevice,newdevice)
+	}
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "not found"})
+}
+func main() {
+	router := gin.Default()
+	router.GET("/", getweatherdevice)
+	router.POST("/", addweatherdevice)
+	router.DELETE("/:id", deletebyId)
 
-		context.IndentedJSON(http.StatusCreated,newdevice)
-	}
-
-	func deleteby Id(context *gin.context){
-		id:=c.Param("id")
-		for i,p:=range newdevice{
-			if p.ID==id{
-				newdevice=append(newdevice[:i]newdevice[i+1:]...)
-				c.IndentedJSON(http.StatusOK, gin.H{"message":"deleted"})
-				return
-			}
-		}
-		c.IndentedJSON(http.StatusNotFound,gin.H{"message":"not found"})
-	}
-	func main(){
-		router:=gin.Deafult()
-		router.GET("/",getweatherdevice)
-		router.POST("/",addweatherdevice)
-		router.DELETE("/",deleteby id)
-		
-		router.Run("localhost:8080")
-	}
+	router.Run("localhost:8080")
+}
